6053a93ce346a316bfd24cf1210d89a24a091ff3
George Chou <george_chou@gss.com.tw>
2020-02-26 08:24:07 +0800

    Fix implementation discovered by unit tests
    1. Add unit test for testing checkin disabled.
    
    2. Added java.time.Clock to CheckinService as a dependency.
    
    3. Changed property injection to setter injection, so that dependencies
    can be changed dynamically for unit test mocking.

--- src/test/java/org/dream/scheduled/tasks/unit/tests/CheckinServiceUnitTest.java
+++ src/test/java/org/dream/scheduled/tasks/unit/tests/CheckinServiceUnitTest.java
@@ -1,15 +1,22 @@
 package org.dream.scheduled.tasks.unit.tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 
 import org.dream.scheduled.tasks.configuration.properties.CheckinConfigurationProperties;
+import org.dream.scheduled.tasks.dto.CheckinParamsDto;
 import org.dream.scheduled.tasks.service.CheckinService;
 import org.dream.scheduled.tasks.service.HttpService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -31,18 +38,15 @@ class CheckinServiceUnitTest {
     @InjectMocks
     private CheckinService checkinService;
     
-    @BeforeEach
-    private void before() throws IOException {
-        when(checkinConfigurationProperties.getTokenUrl()).thenReturn("");
-    }
-    
 	@Test
 	public void givenUsernameAndSecret_whenGetBearerToken_thenCallsHttpServiceOnce() throws IOException {
 	    // Arrange
+	    // 呼叫 getBearerToken 所需要的參數
 	    String username = "test_user";
 	    String secret = "test_secret";
 	    
-	    String destination = "";
+	    // 實際呼叫相依物件 HttpService 的 makeUrlEncodedPost 時會收到的參數
+	    String destination = null;
 	    String queryString = "grant_type=password&username=test_user&password=test_secret";
 	    boolean requiresToken = false;
 	    String token = null;
@@ -52,7 +56,27 @@ class CheckinServiceUnitTest {
 	    
 	    // Assert
 	    verify(httpService, times(1)).makeUrlEncodedPost(destination, queryString, requiresToken, token);
+	}
+	
+	@Test
+	public void givenCheckinNotEnabled_whenCheckin_thenReturnsNotEnabled() throws IOException {
+	    // Arrange
 	    
+	    // 讓這個測試不要被系統時間綁住
+	    Clock clock = Clock.fixed(LocalDateTime.of(2020,2,26,8,40,0,0).toInstant(ZoneOffset.UTC), ZoneId.of("UTC"));
+	    checkinService.setClock(clock);
+	    
+	    CheckinParamsDto checkinParams = mock(CheckinParamsDto.class);
+	    when(checkinConfigurationProperties.isEnabled()).thenReturn(false);
+	    when(checkinConfigurationProperties.getHolidays()).thenReturn("");
+	    when(checkinConfigurationProperties.getCheckinTime()).thenReturn("08:30");
+	    String expectedResult = "自動打卡功能未開啟";
+	    
+	    // Act
+	    String result = checkinService.checkin(checkinParams);
+	    
+	    // Assert
+	    assertEquals(expectedResult,result);
 	}
 
 }
