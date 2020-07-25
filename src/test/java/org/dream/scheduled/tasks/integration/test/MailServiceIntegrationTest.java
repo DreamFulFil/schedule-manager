cc9c5b1224c87f1bbf1ae070a8616162c153c38e
George-Chou <george_chou@gss.com.tw>
2020-07-25 10:06:10 +0800

    Add different db for testing

--- src/test/java/org/dream/scheduled/tasks/integration/test/MailServiceIntegrationTest.java
+++ src/test/java/org/dream/scheduled/tasks/integration/test/MailServiceIntegrationTest.java
@@ -11,10 +11,10 @@ import org.dream.scheduled.tasks.service.MailService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@Profile(value = "test")
+@ActiveProfiles(value = "test")
 public class MailServiceIntegrationTest {
     
     @Autowired
