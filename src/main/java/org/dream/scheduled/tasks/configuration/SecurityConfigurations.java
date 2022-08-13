cce7b7568f340342aab8df1cd53466b42f994797
George-Chou <george_chou@gss.com.tw>
2022-08-13 16:08:24 +0800

    配合 spring boot 2.7.2 搭配的 security 版本調整設定方式

--- src/main/java/org/dream/scheduled/tasks/configuration/SecurityConfigurations.java
+++ src/main/java/org/dream/scheduled/tasks/configuration/SecurityConfigurations.java
@@ -1,73 +1,83 @@
 package org.dream.scheduled.tasks.configuration;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
-@EnableWebSecurity
-public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+/**
+ * 換成 Spring Security 5.7.0 的設定方式
+ * 詳細請參考官方說明文件: 
+ * https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
+ */
+@Configuration
+public class SecurityConfigurations {
 
-    // 僅開放外部系統呼叫的 REST API Security 設定
-    @Configuration
-    @Order(1)
-    public static class RestApiSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors(c -> {
+            CorsConfigurationSource source = request -> {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+                config.setAllowedOrigins(List.of("http://localhost:4200"));
+                config.setAllowedMethods(List.of("GET", "POST"));
+                return config;
+            };
+            c.configurationSource(source);
+        });
+
+        http
+          .requestMatchers()
+              .antMatchers(
+                  "/task/**",
+                  "/heartbeat/**"
+              )
+          .and()
+          .authorizeRequests()
+              .antMatchers("/task/**").hasAuthority("REST")
+              .antMatchers("/heartbeat/**").hasAuthority("HEART")
+          .and()
+          
+          .httpBasic()
+          .and()
+          .csrf()
+              .disable();
         
-        @Bean(name = "restApiPasswordEncoder")
-        public PasswordEncoder passwordEncoder() {
-            return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        }
-        
-        @Override
-        public void configure(AuthenticationManagerBuilder auth) throws Exception {
-            // REST API用
-            auth.inMemoryAuthentication()
-                .withUser("remoteUser")
-                  .password(passwordEncoder().encode("remoteSecret")).authorities("REST")
-                .and()
-                .withUser("waker")
-                  .password(passwordEncoder().encode("d2FrZXVw")).authorities("HEART");
-        }
+        return http.build();
+    }
 
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.cors(c -> {
-                CorsConfigurationSource source = request -> {
-                    CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-                    config.setAllowedOrigins(List.of("http://localhost:4200"));
-                    config.setAllowedMethods(List.of("GET", "POST"));
-                    return config;
-                };
-                c.configurationSource(source);
-            });
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        List<UserDetails> userDetails = 
+            Arrays.asList(
+                User.builder()
+                    .username("remoteUser")
+                    .password("{noop}remoteSecret")
+                    .authorities("REST")
+                    .build(),
 
-            http
-              .requestMatchers()
-                  .antMatchers(
-                      "/task/**",
-                      "/heartbeat/**"
-                  )
-              .and()
-              .authorizeRequests()
-                  .antMatchers("/task/**").hasAuthority("REST")
-                  .antMatchers("/heartbeat/**").hasAuthority("HEART")
-              .and()
-              
-              .httpBasic()
-              .and()
-              .csrf()
-                  .disable();
-        }
+                User.builder()
+                    .username("waker")
+                    .password("{noop}d2FrZXVw")
+                    .authorities("HEART")
+                    .build()
+            );
+        return new InMemoryUserDetailsManager(userDetails);
     }
     
 }
